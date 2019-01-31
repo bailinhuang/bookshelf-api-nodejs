30e124dee68947aa4816306e501192e1ffab4e53
Bailin Huang <bailinhuangq@hotmail.com>
2019-01-31 11:34:44 -0600

    FINISHED CRUDS

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 const express = require('express')
 const app = express()
 // const app = require('cors')
+const cors = require('cors')
 const port = 3001
 const hostname = '127.0.0.1'
 const jwt = require('jsonwebtoken')
 const uuid = require('uuid')
-const SECRET_KEY = 'wutup'
-
-// let books = [{'name': 'x', 'author': 'x'}]
-
+const SECRET_KEY = 'wutup' 
 let books = new Map()
 let users = new Map()
+let book
+let index
+let token
+let bookshelf
 
 app.use(express.json())
+app.use(cors())
 
 const verifyToken = (req, res, next) => {
 
   const { headers } = req
   const token = headers['auth-token']
   try {
-    jwt.verify(token, SECRET_KEY, {maxAge: 300000})
+    jwt.verify(token, SECRET_KEY, { maxAge: 300000 })
     next()
-  } catch (err) { 
+  } catch (err) {
     res.status(401).end()
   }
 }
@@ -34,22 +37,22 @@ app.get('/v2', (req, res) => {
 
 app.post('/v2', (req, res) => {
   res.send('Hello !')
-}) 
+})
 
+//GET ALL BOOKSHELF
 app.get('/v2/book', (req, res) => {
   const { headers } = req
   const token = headers['auth-token']
   const decoded = jwt.decode(token)
   const customer = decoded.customer
-  id = req.body.id
-  console.log(customer)
-  customer_bookshelf = books.get(customer)
-  console.log(customer_bookshelf)
+  id = req.body.id 
+  customer_bookshelf = books.get(customer) 
   res.setHeader('Content-type', 'application/json')
   res.send(customer_bookshelf)
 })
 
-app.post('/v2/book', (req, res) => {
+//ADD A BOOK
+app.post('/v2/book', verifyToken, (req, res) => {
   const { headers } = req
   const token = headers['auth-token']
   const decoded = jwt.decode(token)
@@ -59,24 +62,42 @@ app.post('/v2/book', (req, res) => {
     id: id,
     name: req.body.name,
     author: req.body.author
-  }
-  console.log(books.get(customer))
-  books.get(customer).push(book) 
+  } 
+  books.get(customer).push(book)
   res.setHeader('Content-type', 'application/json')
   res.send(books)
-}) 
+})
 
-app.put('/v2/book/', (req, res) => {
+//UPDATE A BOOK
+app.put('/v2/book/:id', (req, res) => {
   const { headers } = req
   const token = headers['auth-token']
   const decoded = jwt.decode(token)
   const customer = decoded.customer
-  id = req.body.id
-  bookshelf = books.get(customer)
+  id = req.params.id 
+  bookshelf = books.get(customer) 
+  index = bookshelf.findIndex(x => x.id === id)
+  book = bookshelf[index]
+  book.name = req.body.name
+  book.author = req.body.author
+  bookshelf[index] = book
   res.setHeader('Content-type', 'application/json')
   res.send(bookshelf)
 })
 
+//DELETE A BOOK
+app.delete('/v2/book/:id', (req, res) => {
+  const { headers } = req
+  const token = headers['auth-token']
+  const decoded = jwt.decode(token)
+  const customer = decoded.customer
+  id = req.params.id 
+  bookshelf = books.get(customer) 
+  index = bookshelf.findIndex(x => x.id === id)
+  bookshelf.splice(index, 1)
+  res.setHeader('Content-type', 'application/json')
+  res.send(bookshelf)
+})
 
 //REGISTER A USER
 app.post('/v2/user', (req, res) => {
@@ -85,41 +106,39 @@ app.post('/v2/user', (req, res) => {
     password: req.body.password,
     customer: req.body.customer
   }
-  let token = {
+  token = {
     token: jwt.sign(user, SECRET_KEY)
   }
-  users.set(user.username, user)
-  console.log(user.customer)
-  books.set(user.customer, [])
-  console.log(books.get(user.customer))
+  users.set(user.username, user) 
+  books.set(user.customer, []) 
   res.setHeader('Content-type', 'application/json')
   res.send(token)
 })
 
-app.post('/v2/user/login', (req, res) => {
-  console.log(req.body.username)
-  const customer = users.get(req.body.username).customer
-  console.log('login customer: ' + customer)
+//LOGIN A USER AND RETURNS TOKEN
+app.post('/v2/user/login', (req, res) => { 
+  const customer = users.get(req.body.username).customer 
   const user = {
     // username: req.body.username,
     // password: req.body.password,
     customer: customer
   }
-  let token = {
+  token = {
     token: jwt.sign(user, SECRET_KEY, { expiresIn: '300000' })
-  } 
+  }
   res.setHeader('Content-type', 'application/json')
   res.send(token)
 })
 
+//RENEW TOKEN
 app.post('/v2/user/renew', (req, res) => {
   const user = {
     username: req.body.username,
     password: req.body.password,
   }
-  let token = {
+  token = {
     token: jwt.sign(user, SECRET_KEY, { expiresIn: '300000' })
-  } 
+  }
   res.setHeader('Content-type', 'application/json')
   res.send(token)
 })
